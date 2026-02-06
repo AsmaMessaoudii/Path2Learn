68604cacf3f730316a620b87d1c370e3c53d5e56
AsmaMessaoudii <asma.messaoudi@gmail.com>
2026-02-06 23:04:37 +0100

    2end commit

---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Portfolio;
+use App\Form\PortfolioType;
+use App\Repository\PortfolioRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PortfolioController extends AbstractController
+{
+    #[Route('/portfolio', name: 'app_portfolio')]
+    public function index(PortfolioRepository $portfolioRepository): Response
+    {
+        $portfolios = $portfolioRepository->findAll();
+
+        return $this->render('portfolio/index.html.twig', [
+            'portfolios' => $portfolios,
+        ]);
+    }
+    #[Route('/portfolio/new', name: 'portfolio_newPortfolio')]
+public function new(Request $request, EntityManagerInterface $em): Response
+{
+    $portfolio = new Portfolio();
+
+    $form = $this->createForm(PortfolioType::class, $portfolio);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($portfolio);
+        $em->flush();
+
+        return $this->redirectToRoute('app_portfolio');
+    }
+
+    return $this->render('portfolio/CreatePortfolio.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+
+}
---
+++ src/Form/PortfolioType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Portfolio;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PortfolioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description', TextareaType::class)
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Portfolio::class,
+        ]);
+    }
+}
