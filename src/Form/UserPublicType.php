6ae98a412e7bce391c3cffc5e1240fcd50af021b
nawres <nawresjouini38@gmail.com>
2026-02-10 20:08:01 +0100

    3rd commit

--- src/Form/UserPublicType.php
+++ src/Form/UserPublicType.php
@@ -8,9 +8,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserPublicType extends AbstractType
@@ -20,21 +23,72 @@ class UserPublicType extends AbstractType
         $builder
             ->add('nom', TextType::class, [
                 'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Votre nom',
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre nom',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Le nom ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                ],
             ])
             ->add('prenom', TextType::class, [
                 'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Votre prénom',
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre prénom',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Le prénom doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Le prénom ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                ],
             ])
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'votre.email@exemple.com',
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre adresse email',
+                    ]),
+                    new Email([
+                        'message' => 'L\'adresse email "{{ value }}" n\'est pas valide',
+                    ]),
+                ],
             ])
-            // ✅ Use a plainPassword field instead of motDePasse
             ->add('plainPassword', PasswordType::class, [
                 'label' => 'Mot de passe',
-                'mapped' => false, // important! do not map directly to entity
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => '••••••••',
+                    'class' => 'form-control',
+                    'autocomplete' => 'new-password'
+                ],
                 'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
                     new Length([
                         'min' => 5,
-                        'minMessage' => 'Le mot de passe doit contenir au moins 5 caractères',
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
                     ]),
                     new Regex([
                         'pattern' => '/[A-Z]/',
@@ -44,13 +98,22 @@ class UserPublicType extends AbstractType
                         'pattern' => '/[a-z]/',
                         'message' => 'Le mot de passe doit contenir au moins une lettre minuscule',
                     ]),
+                   
                 ],
             ])
             ->add('role', ChoiceType::class, [
                 'label' => 'Rôle',
                 'choices' => [
-                    'Enseignant' => UserRole::TEACHER,  // ✅ Correct
-'Étudiant' => UserRole::STUDENT,    // ✅ Correct
+                    'Enseignant' => UserRole::TEACHER,
+                    'Étudiant' => UserRole::STUDENT,
+                ],
+                'attr' => [
+                    'class' => 'form-select'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez sélectionner un rôle',
+                    ]),
                 ],
             ]);
     }
